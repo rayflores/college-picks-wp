578e551d2d8d3371709edfac8b18b3ec6ccd70eb
rayflores <rayflores88@gmail.com>
2025-09-06 10:37:44 -0400

    adding background for rankings table

--- page-rankings.php
+++ page-rankings.php
@@ -35,34 +35,36 @@ if ( ! function_exists( 'cp_render_trend_icon' ) ) {
 
 $cache_key = 'cp_ap_top_25_rankings_results';
 $teams     = cp_get_data_from_cache( $cache_key );
-?>
+
+?><style>
+.table-dark .logo-cell {
+	background-color: var(--bs-table-bg-type, #222);
+}
+</style>
 <div class="container rankings-page my-4">
 	<h1 class="mb-4">AP Top 25 Rankings</h1>
 	<div class="table-responsive">
-	<table class="table table-dark table-striped table-bordered align-middle">
-		<thead class="table-dark">
-			<tr>
-				<th>Rank</th>
-				<th>Logo</th>
-				<th>Team Name</th>
-				<th>Abbr</th>
-				<th>Mascot Name</th>
-				<th>Team ID</th>
-				<th>Location</th>
-				<th>Color</th>
-				<th>Points</th>
-				<th>Trend</th>
-				<th>Record</th>
-			</tr>
-		</thead>
-		<tbody class="table-dark">
-		<?php
-		if ( ! empty( $teams ) ) :
-			foreach ( $teams as $team ) :
-				?>
+		<table class="table table-dark table-striped table-bordered align-middle text-center">
+			<thead class="table-dark">
+				<tr>
+					<th>Rank</th>
+					<th>Logo</th>
+					<th>Team Name</th>
+					<th>Abbr</th>
+					<th>Mascot Name</th>
+					<th>Points</th>
+					<th>Trend</th>
+					<th>Record</th>
+				</tr>
+			</thead>
+			<tbody class="table-dark">
+<?php
+if ( ! empty( $teams ) ) :
+	foreach ( $teams as $team ) :
+		?>
 				<tr data-team-id="<?php echo esc_attr( $team['team_id'] ); ?>">
 					<td><?php echo isset( $team['rank'] ) ? esc_html( $team['rank'] ) : ''; ?></td>
-					<td>
+					<td class="logo-cell" style="--bs-table-bg-type:#<?php echo esc_attr( $team['team_color'] ); ?>!important;">
 						<?php if ( ! empty( $team['team_logo'] ) ) : ?>
 							<img src="<?php echo esc_url( $team['team_logo'] ); ?>" alt="<?php echo esc_attr( $team['team_name'] ); ?> logo" style="height:32px;vertical-align:middle;" />
 						<?php endif; ?>
@@ -70,21 +72,18 @@ $teams     = cp_get_data_from_cache( $cache_key );
 					<td><?php echo esc_html( $team['team_name'] ); ?></td>
 					<td><?php echo esc_html( $team['team_abbreviation'] ); ?></td>
 					<td><?php echo esc_html( $team['team_display_name'] ); ?></td>
-					<td><?php echo esc_html( $team['team_id'] ); ?></td>
-					<td><?php echo esc_html( $team['team_location'] ); ?></td>
-					<td><span style="background:#<?php echo esc_attr( $team['team_color'] ); ?>;padding:2px 8px;border-radius:3px;color:#fff;"> <?php echo esc_html( $team['team_color'] ); ?> </span></td>
 					<td><?php echo esc_html( $team['points'] ); ?></td>
 					<td><?php echo wp_kses_post( cp_render_trend_icon( $team['trend'] ?? '-' ) ); ?></td>
 					<td><?php echo esc_html( $team['record'] ); ?></td>
 				</tr>
-				<?php
-			endforeach;
-		else :
-			?>
-			<tr><td colspan="11">No rankings found.</td></tr>
-		<?php endif; ?>
-		</tbody>
-	</table>
+		<?php
+	endforeach;
+else :
+	?>
+				<tr><td colspan="11">No rankings found.</td></tr>
+<?php endif; ?>
+			</tbody>
+		</table>
 	</div>
 </div>
 <?php get_footer(); ?>
